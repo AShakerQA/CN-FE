0b43de37876903e810b1f9a2bb0e33bd237e4457
Jordan <jordan.harrison@qa.com>
2020-10-06 16:20:11 +0100

    added createElement example

--- 05_DOM_Manipulation/js/index.js
+++ 05_DOM_Manipulation/js/index.js
@@ -1,7 +1,17 @@
 const header = document.getElementById("heading");
 const input = document.querySelector("#myInput");
 
+const newText = document.getElementById("newText");
+const output = document.getElementById("output");
+
 function update() {
     // debugger;
     header.innerText = input.value;
 }   
+
+function addNew() {
+    const newP = document.createElement("p");
+    newP.innerText = newText.value;
+
+    output.appendChild(newP);
+}
