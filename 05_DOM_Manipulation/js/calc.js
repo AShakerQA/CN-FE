a0ef931bc264d5aa81744301435fa1e6899dfb39
Jordan <jordan.harrison@qa.com>
2020-10-06 16:36:08 +0100

    added history to calculator

--- 05_DOM_Manipulation/js/calc.js
+++ 05_DOM_Manipulation/js/calc.js
@@ -1,31 +1,47 @@
 const num1 = document.getElementById("firstNum");
 const num2 = document.getElementById("secondNum");
 const result = document.getElementById("result");
+const myHistory = document.getElementById("history");
+
+function getValues() {
+    return [Number.parseFloat(num1.value), Number.parseFloat(num2.value)];
+}
+
+function writeResult(newResult) {
+    result.value = newResult;
+}
+
+function addHistory(a, op, b, newResult) {
+    const newHistory = document.createElement('p');
+    newHistory.innerText = `${a} ${op} ${b} = ${newResult}`;
+
+    myHistory.appendChild(newHistory);
+}
 
 function add() {
-    const a = Number.parseFloat(num1.value);
-    const b = Number.parseFloat(num2.value);
-
-    result.value = a + b;
+    const [a, b] = getValues(); //https://portal.qa-community.co.uk/~/cne/learning/javascript/javascript--destructuring#overview
+    const newResult = a + b;
+    writeResult(newResult);
+    addHistory(a, '+', b, newResult);
 }
 
 function subtract() {
-    const a = Number.parseFloat(num1.value);
-    const b = Number.parseFloat(num2.value);
-
-    result.value = a - b;
+    const [a, b] = getValues();
+    const newResult = a - b;
+    writeResult(newResult);
+    addHistory(a, '-', b, newResult);
 }
 
 function multiply() {
-    const a = Number.parseFloat(num1.value);
-    const b = Number.parseFloat(num2.value);
-
-    result.value = a * b;
+    const [a, b] = getValues();
+    const newResult = a * b;
+    writeResult(newResult);
+    addHistory(a, '*', b, newResult);
 }
 
 function divide() {
-    const a = Number.parseFloat(num1.value);
-    const b = Number.parseFloat(num2.value);
-
-    result.value = a / b;
+    const [a, b] = getValues();
+    const newResult = a / b;
+    writeResult(newResult);
+    addHistory(a, '/', b, newResult);
 }
