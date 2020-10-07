2aab2e2281a0e3bb3311ed8bd274101073b4264f
Jordan <jordan.harrison@qa.com>
2020-10-07 15:03:20 +0100

    added requests

--- 06_Requests/js/index.js
+++ 06_Requests/js/index.js
@@ -1,8 +1,10 @@
+const output = document.getElementById("output");
+let data;
 const request = new XMLHttpRequest();
 request.open("GET", "https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/example.json");
-request.setRequestHeader("accept", "application/json");
-request.onload = function() {
-    console.log(request.response);
+request.onload = function () {
+    console.log("Response received!");
+    data = request.response;
 }
 request.send();
-console.log("Request sent!");
+console.log("Request Sent!");
